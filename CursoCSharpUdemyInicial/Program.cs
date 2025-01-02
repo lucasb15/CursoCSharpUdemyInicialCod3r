4da5ecc5959d8f43dfdbf8d3f2e97c9f9eea56a9
Lucas Gomes Santos <lgsantos306@gmail.com>
2025-01-02 14:05:13 -0300

    Adicionar arquivos de projeto.

---
+++ CursoCSharpUdemyInicial/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CursoCSharpUdemyInicial.Fundamentos;
+
+namespace CursoCSharpUdemyInicial
+{
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
