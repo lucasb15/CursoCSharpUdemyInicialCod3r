d0d6bb7d786d2eef27557fb3d4a776024ef2c4e7
Lucas Gomes Santos <lgsantos306@gmail.com>
2025-01-02 21:18:30 -0300

    Fundamentos - lendo dados do console

--- CursoCSharpUdemyInicial/Program.cs
+++ CursoCSharpUdemyInicial/Program.cs
@@ -14,6 +14,7 @@ namespace CursoCSharpUdemyInicial
                 {"Inferência - Fundamentos", Inferencia.Executar},
                 {"Interpolação - Fundamentos", Interpolacao.Executar},
                 {"Notação Ponto - Fundamentos", NotacaoPonto.Executar},
+                {"Lendo Dados - Fundamentos", LendoDados.Executar},
             });
 
             central.SelecionarEExecutar();
